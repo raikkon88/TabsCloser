ff0d8ceab0c5eac63bf3aa25d2318238bb991835
Marc Sanchez <marc.sanchez@launchmetrics.com>
2021-03-16 12:05:35 +0100

    Add both functionality, ringcentral and zoom

--- background.js
+++ background.js
@@ -9,4 +9,14 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
             }
         });
     });
+    chrome.tabs.query({ url: "https://*.ringcentral.com/*/*" }, tabs => {
+        tabs.forEach(tab => {
+            console.log("found -> ", tab.url);
+            if (tab.url.includes("post_meeting")) {
+                console.log("includes");
+                chrome.tabs.remove(tab.id, () =>
+                    console.log(`closed ${tab.url}`));
+            }
+        });
+    });
 });
