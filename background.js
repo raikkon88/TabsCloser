8ddef3cfe62dc18800abccce56988a3c954b4f8a
Marc Sanchez <marc.sanchez@launchmetrics.com>
2021-03-16 12:01:30 +0100

    Change to zoom url extension

--- background.js
+++ background.js
@@ -1,10 +1,11 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab)  {
-    chrome.tabs.query({url: "https://*.ringcentral.com/*/*"}, tabs => {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    chrome.tabs.query({ url: "https://launchmetrics.zoom.us/*/*" }, tabs => {
         tabs.forEach(tab => {
             console.log("found -> ", tab.url);
-            if(tab.url.includes("post_meeting")){
+            if (tab.url.includes("success")) {
                 console.log("includes");
-                chrome.tabs.remove(tab.id, () => console.log(`closed ${tab.url}`));
+                chrome.tabs.remove(tab.id, () =>
+                    console.log(`closed ${tab.url}`));
             }
         });
     });
